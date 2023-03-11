5bd27ebbd6e71464149de23afaf4daa4dfdbc20e
srijan <srijandubey3105@gmail.com>
2023-03-12 00:22:08 +0530

    adding allTodo type from router output

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,13 @@
 import { z } from "zod";
+//in order to get the type of todo
+// export type Todo = z.infer<typeof todoInput>;
+import type { inferRouterOutputs } from "@trpc/server";
+import type { AppRouter } from "./server/api/root";
+
+type RouterOutputs = inferRouterOutputs<AppRouter>;
+type allTodosOutput = RouterOutputs["todo"]["all"]
+
+export type Todo = allTodosOutput[number]
 
 
 export const todoInput = z
@@ -7,4 +16,3 @@ export const todoInput = z
     })
     .min(1)
     .max(100)
-    
