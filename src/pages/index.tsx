9b6a52e80788ac7785d3800e5eebb99361060522
srijan <srijandubey3105@gmail.com>
2023-03-13 23:22:12 +0530

    changed tailwind classes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,18 +15,16 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-          {sessionData && (
-            <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-              <div className="flex flex-col gap-4 rounded-xl bg-white/10 p-4 text-white">
-                <h3 className="text-xl font-bold ">
-                  <Todos />
-                </h3>
+        {sessionData && (
 
-              </div>
-            </div>
-          )}
-        </div>
+          <div className="flex flex-col gap-4 rounded-xl bg-white/10 p-4 text-white">
+            <h3 className="text-xl font-bold ">
+              <Todos />
+            </h3>
+
+          </div>
+        )}
+
         <div className="flex flex-col items-center gap-2">
           <p className="text-2xl text-white">
 
